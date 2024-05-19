9a01c4f545a948ab2f2c7ff548be7ab9a876c182
Sayaka Tomie <sayakatomie@SayakanoMacBook-Air.local>
2024-05-19 21:35:01 +0900

    temp:テスト

--- public/index.php
+++ public/index.php
@@ -2,15 +2,18 @@
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
-echo 'test';
 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
+
+
 }
 
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
+
+echo 'test'; 
 
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/../bootstrap/app.php')
