fc343b586d78c06267c0c945aa93f1f972de8ed4
Sayaka Tomie <sayakatomie@SayakanoMacBook-Air.local>
2024-05-19 23:03:42 +0900

    temp:テスト

--- public/index.php
+++ public/index.php
@@ -1,17 +1,41 @@
 <?php
+echo 'Inside public/index.php start'; 
 
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
+
+echo 'Before maintenance check'; 
 
 // Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
+echo 'Before autoload'; 
+
 // Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
+echo 'Before app bootstrap'; 
+
 // Bootstrap Laravel and handle the request...
-(require_once __DIR__.'/../bootstrap/app.php')
-    ->handleRequest(Request::capture());
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+echo 'After app bootstrap'; 
+
+$kernel = $app->make('Illuminate\Contracts\Http\Kernel');
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+echo 'After kernel handle'; 
+
+$response->send();
+
+echo 'After response send'; 
+
+$kernel->terminate($request, $response);
+
+echo 'After kernel terminate'; 
