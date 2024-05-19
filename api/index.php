191dc8431468022e120220c9fe98319c50a0e22b
Sayaka Tomie <sayakatomie@SayakanoMacBook-Air.local>
2024-05-19 20:58:30 +0900

    fix: api/index.php

--- api/index.php
+++ api/index.php
@@ -1,6 +1,16 @@
 <?php
-/**
-* Here is the serverless function entry
-* for deployment with Vercel.
-*/
-require __DIR__.'/../public/index.php';
+
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+require __DIR__.'/../vendor/autoload.php';
+
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+// Create a Laravel request from the incoming Vercel request
+$request = Request::capture();
+// Bootstrap the Laravel application
+$app->make(Kernel::class)->handle($request);
+
