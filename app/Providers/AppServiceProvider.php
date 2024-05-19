b333a1e47849dde70551ac67fdc2e9f09195d366
Sayaka Tomie <sayakatomie@SayakanoMacBook-Air.local>
2024-05-19 21:30:26 +0900

    temp:テスト

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Contracts\Http\Kernel;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->app->make(Kernel::class)->prependMiddleware(MyMiddleware::class);
+        //
     }
 }
--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-
+echo 'test';
 use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
