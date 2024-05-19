5cc60fd3277505c6355b007a2d7a706b98484c4a
Sayaka Tomie <sayakatomie@SayakanoMacBook-Air.local>
2024-05-19 22:36:37 +0900

    temp:テスト

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,20 +5,34 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-return Application::configure(basePath: dirname(__DIR__))
+echo 'Before Application::configure'; 
+
+$app = Application::configure(basePath: dirname(__DIR__));
+echo 'After Application::configure'; 
+
+$app = $app
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     );
-    echo 'Inside app.php 1'
+echo 'After withRouting'; 
 
+$app = $app
     ->withMiddleware(function (Middleware $middleware) {
         //
     });
-    echo 'Inside app.php 2'
+echo 'After withMiddleware'; 
 
+$app = $app
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    });
+echo 'After withExceptions'; 
 
+$app = $app->create();
+echo 'After create'; 
+
+return $app;
+
+echo 'Inside app.php end';
